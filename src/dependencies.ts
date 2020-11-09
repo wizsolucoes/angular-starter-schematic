5aaf73a8e98fde0fdef34342fa787633f9647918
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-11-09 16:52:31 -0300

    chore: Bulma retirado da lista de dependências

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -2,7 +2,6 @@ export const dependencies: { [key: string]: string } = {
   "@microsoft/applicationinsights-web": "^2.5.7",
   "@wizsolucoes/ngx-wiz-sso": "^2.0.1",
   "@wizsolucoes/wiz-loader": "^2.1.0",
-  bulma: "^0.9.0",
   "ng2-currency-mask": "^9.0.2",
   "ngx-mask": "^9.1.2",
 };
