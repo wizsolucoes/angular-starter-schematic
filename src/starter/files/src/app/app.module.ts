a58bcca6e70e0669aaa0b0df54c2f85d60e1f6bc
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-11-12 11:02:22 -0300

    chore: código do AppInsights removido

--- src/starter/files/src/app/app.module.ts
+++ src/starter/files/src/app/app.module.ts
@@ -5,14 +5,12 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
   LOCALE_ID,
   NgModule,
-  ErrorHandler,
 } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { DefaultInterceptor } from './core/interceptors/default.interceptor';
 import { SharedModule } from './shared/shared.module';
-import { ErrorHandlerService } from './core/services/monitoring/error-handler.service';
 
 registerLocaleData(localePt, 'pt-BR');
 
@@ -22,7 +20,6 @@ registerLocaleData(localePt, 'pt-BR');
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
     { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
-    { provide: ErrorHandler, useClass: ErrorHandlerService },
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
