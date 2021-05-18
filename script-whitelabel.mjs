b1d3fdcb77af6fb9c2083e636daaf85294ca8d2b
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2021-05-18 16:33:11 -0300

    feat: add scripts for testing schematics

---
+++ script-whitelabel.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env zx
+
+let version = await $`node -p "require('./package.json').version"`;
+
+await $`npm install`;
+await $`npm run build`;
+$`npm pack`;
+
+cd('..');
+
+await $`ng new schematics-test-app --style=scss`;
+cd('../schematics-test-app');
+
+await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
+await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=true`;
+
+await $`ng test`;
+
+await $`ng lint`;
---
+++ script.mjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env zx
+
+let version = await $`node -p "require('./package.json').version"`;
+
+await $`npm install`;
+await $`npm run build`;
+$`npm pack`;
+
+cd('..');
+
+await $`ng new schematics-test-app --style=scss`;
+cd('../schematics-test-app');
+
+await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
+await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=false`;
+
+await $`ng test --no-watch --code-coverage`;
+await $`ng lint`;
