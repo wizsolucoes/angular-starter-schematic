280713b378332acfb1e2d2b4ad123acee9fca203
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2021-05-20 16:32:16 -0300

    test: testing pipeline

--- test-generated-app.mjs
+++ test-generated-app.mjs
@@ -1,18 +1,18 @@
 #!/usr/bin/env zx
 
-let version = await $`node -p "require('./package.json').version"`;
+// let version = await $`node -p "require('./package.json').version"`;
 
-await $`npm install`;
-await $`npm run build`;
-$`npm pack`;
+// await $`npm install`;
+// await $`npm run build`;
+// $`npm pack`;
 
-cd('..');
+// cd('..');
 
-await $`ng new schematics-test-app --style=scss`;
-cd('../schematics-test-app');
+// await $`ng new schematics-test-app --style=scss`;
+// cd('../schematics-test-app');
 
-await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
-await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=false`;
+// await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
+// await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=false`;
 
-await $`ng test --no-watch --code-coverage`;
-await $`npx ng lint`;
+// await $`ng test --no-watch --code-coverage`;
+// await $`npx ng lint`;
