279a5df0fbf29caa605d9861ff10719a07e3ca7c
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2021-05-20 17:07:52 -0300

    test: testing pipeline7

--- test-generated-app-whitelabel.mjs
+++ test-generated-app-whitelabel.mjs
@@ -1,17 +1,17 @@
 #!/usr/bin/env zx
 
-// let version = await $`node -p "require('./package.json').version"`;
+let version = await $`node -p "require('./package.json').version"`;
 
-// await $`npm install`;
-// await $`npm run build`;
-// $`npm pack`;
+await $`npm install`;
+await $`npm run build`;
+$`npm pack`;
 
-// cd('..');
+cd('..');
 
-// await $`ng new schematics-whitelabel-test-app --style=scss`;
-// cd('../schematics-whitelabel-test-app');
+await $`ng new schematics-whitelabel-test-app --style=scss`;
+cd('../schematics-whitelabel-test-app');
 
-// await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
+await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
 // await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=true`;
 
 // await $`ng test --no-watch --code-coverage`;
