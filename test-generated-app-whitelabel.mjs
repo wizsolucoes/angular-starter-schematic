50a3266555035791b6c919e643d9a09ab9b44653
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2021-05-20 17:36:00 -0300

    test: testing pipeline10

--- test-generated-app-whitelabel.mjs
+++ test-generated-app-whitelabel.mjs
@@ -1,19 +1,19 @@
 #!/usr/bin/env zx
 
-let version = await $`node -p "require('./package.json').version"`;
+// let version = await $`node -p "require('./package.json').version"`;
 
-await $`npm install`;
-await $`npm run build`;
-$`npm pack`;
+// await $`npm install`;
+// await $`npm run build`;
+// $`npm pack`;
 
-cd('..');
+// cd('..');
 
-await $`ng new schematics-whitelabel-test-app --style=scss`;
-cd('../schematics-whitelabel-test-app');
+// await $`ng new schematics-whitelabel-test-app --style=scss`;
+// cd('../schematics-whitelabel-test-app');
 
-await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
-await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=true`;
+// await $`npm i --no-save ${__dirname}/wizsolucoes-angular-starter-${version}.tgz`;
+// await $`ng g @wizsolucoes/angular-starter:ng-add --white-label=true`;
 
-await $`ng test --no-watch --code-coverage`;
+// await $`ng test --no-watch --code-coverage`;
 
-// await $`ng lint`;
+// // await $`ng lint`;
